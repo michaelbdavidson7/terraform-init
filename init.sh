9eb2f8dca96cf64e0f7d3d5e18e2d2d991c4ed30
mike davidson <mikedavidson613@gmail.com>
2019-05-12 16:16:44 -0400

    Better comments

--- init.sh
+++ init.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
+# he says we need python 2
 python --version
 apt-get update
 apt install python pip -y
-
 pip install --upgrade pip
+
+# install terraform
 curl -O https://releases.hashicorp.com/terraform/0.11.2/terraform_0.11.2_linux_amd64.zip
 mkdir /bin/terraform
 unzip terraform_0.11.2_linux_amd64.zip /bin/terraform
-
 export PATH=$PATH:/bin/terraform
 terraform --version
 
-
+# install awscli
 pip install awscli --upgrade 
 apt-get update
+
+# install ansible
 apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install ansible -y 
 ansible --version
 
-# he makes a directory to do stuff install
+# he makes a directory to do stuff with the install
 mkdir terraform-scripts
 cd terraform-scripts
 
-# generate key
+# he generates a key
 #ssh-keygen
 #/root/.ssh/kryptonite
 #ssh-agent bash
